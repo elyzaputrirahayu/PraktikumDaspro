abead276d4e8d78e7bb097a23cb89c00c7b1c2fd
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-09-23 11:51:09 +0700

    perubahan

--- TugasKolaborasi08.java
+++ TugasKolaborasi08.java
@@ -1,6 +1,7 @@
 public class TugasKolaborasi08 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("polinema jossss");
     
     }
 }
