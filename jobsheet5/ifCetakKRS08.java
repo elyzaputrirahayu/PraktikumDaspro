2b3d69eb086081651489126409d0a35be519879a
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-09-30 11:42:32 +0700

    baru

---
+++ jobsheet5/ifCetakKRS08.java
@@ -0,0 +1,16 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class ifCetakKRS08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---CETAK KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
