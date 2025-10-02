dbff8815a0ce7a9be5e6c3771a7f8089094e208e
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-02 08:41:06 +0700

    perubahan

--- jobsheet5/ifCetakKRS08.java
+++ jobsheet5/ifCetakKRS08.java
@@ -1,16 +1,19 @@
-package jobsheet5;
+// package jobsheet5;
 import java.util.Scanner;
 
-public class ifCetakKRS08 {
+public class ifCetakKRS08
+ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("---CETAK KRS SIAKAD---");
+        System.out.println("---CETAK KRS SIAKADjava---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
     }
 }
