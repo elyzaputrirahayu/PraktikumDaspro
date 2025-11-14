60d669f79d806a8452817b4674a64f47cf79fbd4
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-15 00:12:02 +0700

    percobaan 3

---
+++ jobsheet 9/ArrayRataNilai08.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMhs];
+        double totalLulus = 0, totalTidakLulus = 0;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke - " + (i+1) + " : ");
+            nilaiMhs [i] = sc.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
+        }
+
+        double rataLulus = 0, rataTidakLulus = 0;
+        if (jumlahLulus > 0) {
+            rataLulus = totalLulus / jumlahLulus;
+        }
+        if (jumlahTidakLulus > 0) {
+            rataTidakLulus = totalTidakLulus / jumlahTidakLulus;
+        }
+
+        System.out.println();
+        System.out.println("Rata-rata nilai lulus = " +rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+    }
+}
