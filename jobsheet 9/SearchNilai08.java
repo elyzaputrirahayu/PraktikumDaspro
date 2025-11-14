e2f2135129326573a1dd7cc175539fdf8cf9ea76
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-15 00:14:34 +0700

    menjawab pertanyaan percobaan 4

--- jobsheet 9/SearchNilai08.java
+++ jobsheet 9/SearchNilai08.java
@@ -34,3 +34,15 @@ public class SearchNilai08 {
         }
     }
 }
+
+//1. Jelaskan maksud dari statement break; pada baris ke-10 kode program percobaan 4 di atas.
+//   Jawab : Statement break; digunakan untuk menghentikan perulangan (loop) secara paksa meskipun kondisi loop-nya masih memungkinkan untuk dilanjutkan.
+//           break; berfungsi untuk menghentikan perulangan saat kondisi yang dicari sudah terpenuhi, agar program lebih efisien dan cepat.
+
+//2. Modifikasi kode program pada percobaan 4 di atas sehingga program dapat menerima
+//input berupa banyaknya elemen array nilai, isi array, dan sebuah nilai (key) yang ingin
+//dicari. Lalu cetak ke layar indeks posisi elemen dari nilai (key) yang dicari. Contoh hasil program:
+
+//3. Modifikasi program pada percobaan 4 di atas, sehingga program akan memberikan pesan
+//"Nilai yang dicari tidak ditemukan" jika nilai yang dicari (key) tidak ada di dalam array.
+//Contoh tampilan program sebagai berikut:
