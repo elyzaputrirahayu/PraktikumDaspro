ba50e6a8324984f3c3635325e2b6a36c23c59bd5
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-15 00:12:33 +0700

    menjawab pertanyaan

--- jobsheet 9/ArrayRataNilai08.java
+++ jobsheet 9/ArrayRataNilai08.java
@@ -37,3 +37,9 @@ public class ArrayRataNilai08 {
         System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
     }
 }
+
+//1. Modifikasi kode program pada praktikum percobaan 3 di atas (ArrayRataNilaiXX.java) agar program dapat menampilkan banyaknya mahasiswa yang lulus, yaitu mahasiswa yang memiliki lebih besar dari 70 (>70).
+
+//2. Modifikasi program pada praktikum percobaan 3 di atas (ArrayRataNilaiXX.java)sehingga program menerima jumlah elemen berdasarkan input dari pengguna dan mengeluarkan output seperti berikut ini:
+
+//3. Commit dan push hasil modifikasi Anda ke Github dengan pesan “Modifikasi Percobaan 3”
