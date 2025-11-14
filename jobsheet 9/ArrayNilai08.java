820cffe3d02eb5a6b2cbc9184f63055d7e193210
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-15 00:10:39 +0700

    menjawab pertanyaan percobaan 2

--- jobsheet 9/ArrayNilai08.java
+++ jobsheet 9/ArrayNilai08.java
@@ -20,3 +20,17 @@ public class ArrayNilai08 {
 
     }
 }
+
+//1. Ubah statement pada langkah nomor 5 menjadi seperti berikut ini: Jalankan program. Apakah terjadi perubahan? Mengapa demikian?
+//   Jawab : Kedua kondisi (i < nilaiAkhir.length dan i < 10) sama saja hasilnya, karena array memang berisi 10 elemen. Program tetap berjalan normal karena panjang array = 10, dan indeks maksimum = 9.
+
+//2. Apa yang dimaksud dengan kondisi: i < nilaiAkhir.length ?
+//   Jawab : Selama nilai i lebih kecil dari panjang array nilaiAkhir, maka perulangan akan terus berjalan, melakukan perulangan sebanyak jumlah elemen yang ada di array nilaiAkhir
+
+//3. Ubah statement pada langkah nomor 6 menjadi seperti berikut ini, sehingga program hanya menampilkan nilai Mahasiswa yang lulus saja (yaitu mahasiswa yang memiliki nilai> 70): ,Jalankan program dan jelaskan alur program!
+//   Jawab : Program ini membaca nilai 10 mahasiswa, lalu menampilkan mahasiswa mana saja yang lulus (nilai > 70).
+//           Bagian if digunakan untuk seleksi kondisi kelulusan, sedangkan dua loop digunakan untuk input dan pengecekan data.
+
+//4. Modifikasi program agar menampilkan status kelulusan semua mahasiswa berdasarkan nilai, yaitu dengan menampilkan status mana mahasiswa yang lulus dan tidak lulus, seperti ilustrasi output berikut:
+
+//5. Commit dan push hasil modifikasi Anda ke Github dengan pesan “Modifikasi Percobaan 2”
