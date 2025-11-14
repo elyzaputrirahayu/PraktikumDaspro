55eb7a3bbbbde7d50e59cc98d7084acfc65a3d7c
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-14 22:31:23 +0700

    percobaan 1

---
+++ jobsheet 5/ifCetakKRS08.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifCetakKRS08{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---CETAK KRS SIAKADjava---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        }
+        String hasil= (uktLunas) ? "Registrasi berhasil. Silahkan cetak KRS" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu. ";   
+            System.out.println("[versi ternary]" + hasil);
+    }
+}
