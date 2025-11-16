e61cbbbc59f640c4b47d4994e005c2952cb9b318
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-16 19:52:28 +0700

    percobaan 4

--- jobsheet 10/Siakad08.java
+++ jobsheet 10/Siakad08.java
@@ -8,11 +8,26 @@ public class Siakad08 {
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
 
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("nilai mata kuliah " + (j+1) + ": ");
                 nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
             }
+            System.out.println("nilai rata-rata: " + totalPerSiswa/3);
+
+            }
+            System.out.println("\n============================================");
+            System.out.println("Rata-rata nilai setiap mata kuliah");
+
+            for (int j = 0; j < 3; j++) {
+                double totalPerMatkul = 0;
+
+                for (int i = 0; i < 4; i++) {
+                    totalPerMatkul += nilai[i][j];
+                }
+                System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul/4);
         }
     }
 }
