1b561c51d8b7272b94155c11b78554a73764ffde
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-16 15:54:41 +0700

    modifikasi percobaan 2 nomer 4

--- jobsheet 10/BioskopWithScanner08.java
+++ jobsheet 10/BioskopWithScanner08.java
@@ -24,21 +24,33 @@ public class BioskopWithScanner08 {
 
                     System.out.print("Masukkan baris: ");
                     baris = sc.nextInt();
-                    while (baris < 1 || baris > 4) {
-                        System.out.println("baris tidak tersedia !");
-                        System.out.println("Masukkan baris :");
-                        baris = sc.nextInt();
-                    }
+                        while (baris < 1 || baris > 4) {
+                            System.out.println("baris tidak tersedia !");
+                            System.out.println("Masukkan baris :");
+                            baris = sc.nextInt();
+                        }
 
                     System.out.print("Masukkan kolom: ");
                     kolom = sc.nextInt();
-                    while (kolom < 1 || kolom > 2) {
-                        System.out.println("kolom tidak tersedia !");
-                        System.out.println("Masukkan kolom :");
-                        kolom = sc.nextInt();
-                    }
-                    sc.nextLine();
+                        while (kolom < 1 || kolom > 2) {
+                            System.out.println("kolom tidak tersedia !");
+                            System.out.println("Masukkan kolom :");
+                            kolom = sc.nextInt();
+                        }
+                        sc.nextLine();
 
+                        if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2 ) {
+                            System.out.println("kursi tidak tersedia, ulangi !");
+                            continue;
+                        }
+
+                        if (penonton[baris-1][kolom-1] !=null) {
+                            System.out.println("kursi sudah terisi oleh " + penonton[baris-1][kolom-1]);
+                            System.out.println("pilih kursi lain.");
+                            continue;  
+                        }
+                        break;
+                    }
                     penonton[baris-1][kolom-1] = nama;
 
                     System.out.print("Input penonton lain nya? (y/n): ");
@@ -50,7 +62,6 @@ public class BioskopWithScanner08 {
                 }
             }
         }
-    }
 
 // 1. Apakah pengisian elemen array dari scanner harus dilakukan secara berurutan mulai dari indeks ke-0? Jelaskan!
 //    jawab : Tidak, Pengisian elemen array tidak harus dilakukan berurutan dari indeks ke-0. Jadi, pengisian boleh acak, misalnya langsung isi kursi baris 3 kolom 2, tidak harus mulai dari baris 1 kolom 1
@@ -63,5 +74,6 @@ public class BioskopWithScanner08 {
 // 3. Modifikasi kode program untuk menghandle apabila nomor baris/kolom kursi yang tidak tersedia
 
 // 4. Pada menu 1, modifikasi kode program untuk memberikan warning apabila kursi yang dipilih sudah terisi oleh penonton lainnya lalu munculkan perintah untuk memasukkan baris dan kolom kembali
+
 // 5. Pada menu 2, jika kursi kosong, ganti null dengan ***
 // 6. Commit dan push hasil modifikasi Anda ke Github dengan pesan “Modifikasi Percobaan 2”
