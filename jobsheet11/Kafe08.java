b9403b13ac4a4ac92acb091ed615fa1bf0decb66
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-28 23:12:42 +0700

    menjawab pertanyaan percobaan 1

--- jobsheet11/Kafe08.java
+++ jobsheet11/Kafe08.java
@@ -16,10 +16,28 @@ public class Kafe08 {
 
 // 1. Apakah fungsi tanpa parameter selalu harus bertipe void?
 //    jawab : tidak, yang wajib void adalah fungsi yang tidak mengembalikan nilai
+
 // 2. Apakah daftar menu pada program kafe dapat ditampilkan tanpa menggunakan fungsi Menu()? 
 //    Modifikasi kode program tersebut untuk dapat menampilkan daftar menu tanpa menggunakan fungsi!
 //    jawab : Ya, daftar menu pada program kafe bisa ditampilkan tanpa menggunakan fungsi Menu().
+
 // 3. Jelaskan keuntungan menggunakan fungsi Menu() dibandingkan menulis semua
 //    perintah penampilan menu langsung di dalam fungsi main.
+//    jawab : kode lebih rapi terstruktur dan lebih efektif
+
 // 4. Uraikan secara singkat alur eksekusi program ketika fungsi Menu() dipanggil dari main
 //    (mulai dari program dijalankan sampai daftar menu tampil di layar)
+//    jawab : 
+//           1. Program dijalankan
+//              Komputer pertama kali masuk ke fungsi main(), karena semua program Java selalu mulai dari situ.
+
+//           2. Di dalam main, ada perintah Menu(), Begitu komputer membaca baris itu, program berpindah sementara dari main() ke fungsi Menu().
+
+//           3. Komputer pindah ke fungsi Menu() Lalu semua perintah yang ada di dalam Menu() dijalankan satu per satu.
+
+//           4. Menu tampil di layar Setiap System.out.println() yang ada di dalam Menu() langsung muncul di layar.
+//              Jadi daftar menu kopi, teh, roti bakar, semuanya di-print oleh fungsi itu.
+
+//           5. Selesai jalankan Menu(), Setelah semua menu sudah ditampilkan, tugas fungsi Menu() selesai.
+
+//           6. Komputer balik lagi ke main(), Komputer kembali ke baris berikutnya setelah Menu(); tadi.
