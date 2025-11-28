5eeaeb0bd1c1d1bb7fd14d3cc4e55a2fcf20fae4
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-28 22:37:41 +0700

    modifikasi percobaan 1

--- jobsheet11/Kafe08.java
+++ jobsheet11/Kafe08.java
@@ -1,7 +1,7 @@
 package jobsheet11;
 
 public class Kafe08 {
-    public static void Menu() {
+    public static void main(String[] args){
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -12,7 +12,14 @@ public class Kafe08 {
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
-    public static void main(String[] args) {
-        Menu();
-    }
 }
+
+// 1. Apakah fungsi tanpa parameter selalu harus bertipe void?
+//    jawab : tidak, yang wajib void adalah fungsi yang tidak mengembalikan nilai
+// 2. Apakah daftar menu pada program kafe dapat ditampilkan tanpa menggunakan fungsi Menu()? 
+//    Modifikasi kode program tersebut untuk dapat menampilkan daftar menu tanpa menggunakan fungsi!
+//    jawab : Ya, daftar menu pada program kafe bisa ditampilkan tanpa menggunakan fungsi Menu().
+// 3. Jelaskan keuntungan menggunakan fungsi Menu() dibandingkan menulis semua
+//    perintah penampilan menu langsung di dalam fungsi main.
+// 4. Uraikan secara singkat alur eksekusi program ketika fungsi Menu() dipanggil dari main
+//    (mulai dari program dijalankan sampai daftar menu tampil di layar)
