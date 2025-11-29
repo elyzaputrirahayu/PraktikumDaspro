3902dc4e3648f222d35043406c8158d896e0140f
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-29 23:37:33 +0700

    percobaan 4

---
+++ jobsheet11/PengunjungCafe.java
@@ -0,0 +1,16 @@
+package jobsheet11;
+
+public class PengunjungCafe {
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar nama Pengunjung : ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
